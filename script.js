15aa3c865e7717e123d17be586946c75c096ebae
VJHARAN <haranvj1997@gmail.com>
2025-03-22 22:11:34 +0530

    Change colors

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ function playGame(){
             divText=`COMPUTER won! \t ${computerChoice} beats ${humanChoice}`;
             computerScore+=1;
         }   
-        console.log(divText);       
+             
         round.textContent=divText;
         playerScoreSpan.textContent=`Computer: ${computerScore}`;
         computerScoreSpan.textContent=` Player: ${humanScore}`   ;
@@ -56,11 +56,13 @@ function playGame(){
     const container=document.querySelector('#container');
     const btn=document.querySelectorAll('button');
     const div=document.querySelector('#subdiv');
+    const heading=document.querySelector('h1');
     const round=document.createElement('p');
     const score=document.createElement('p');
     let finalResult=document.createElement('p');
-    let divText='';  
-    
+
+    let divText='* click any button to start game *';  
+    round.textContent=divText;
     btn.forEach((button)=>{
         button.addEventListener('click',(e)=>{
             let targetBtn=e.target;
@@ -81,13 +83,24 @@ function playGame(){
     score.appendChild(playerScoreSpan);
     container.style.cssText="display:flex ; flex-direction :column;align-items:center; justify-content:center; gap:20px; height:500px; ";
     btn.forEach((button)=>{
-        button.style.cssText="width:150px; height:50px; margin:20px; border:2px solid  #8874cc; border-style:outset; border-radius:20px; font-size:20px; font-weight:600; background:#b5abe2; color:#4f4477;";
+        button.addEventListener('mouseover',()=>{
+            button.style.background='#4a23d3';
+            button.style.color='white';
+            button.style.border='3px solid black';
+        })
+        button.addEventListener('mouseout',()=>{
+            button.style.background='#f3d0a4';
+            button.style.color='#660e60';
+            button.style.border="2px solid  #8874cc";
+        })
+        button.style.cssText="width:150px; height:50px; margin:20px; border:2px solid  #8874cc; border-style:outset; border-radius:20px; font-size:20px; font-weight:600; background:#f3d0a4; color:#660e60;";
+        
     });
     div.style.cssText="text-align:center;padding:10px 10px;width:520px; height:auto; margin:20px; border:2px solid  #8874cc; border-radius:8px; font-size:20px; font-weight:600; background:#bc5090; color:#000;";
     finalResult.style.cssText="color:white; font-weight:700; font-size:40px;"  ;
     round.style.cssText="color:white; font-weight:800; font-size:30px;  "  ;
-     
-    document.body.style.background="#b8e0d4";
+    heading.style.cssText="background:#fefee3;color:#008585; border-style:outset; padding:8px; border:6px solid #194a7a ;  border-radius: 10px; border-top:1px solid;";
+    document.body.style.background="#cac7ff";
     score.style.cssText="display:flex; align-items:center; justify-content:center; gap:30px; font-weight:900; font-size:25px;";
 }
 
