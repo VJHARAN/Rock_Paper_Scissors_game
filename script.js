10f8aa720030492f2a30eebd9b8cdcde71a90afd
VJHARAN <haranvj1997@gmail.com>
2025-03-22 19:45:32 +0530

    Add a user-interface

--- script.js
+++ script.js
@@ -7,55 +7,75 @@ function getComputerChoice(){
     }
 }
 
-function getHumanChoice(){
-    const userInput=prompt("Enter your choice (rock/paper/scissors) : ")
+// function getHumanChoice(){
+//     const userInput=prompt("Enter your choice (rock/paper/scissors) : ")
      
-    return userInput;
-}
+//     return userInput;
+// }
 
 
 
 function playGame(){
     
-    function playRound(humanChoice,computerChoice,round){
+    function playRound(humanChoice,computerChoice ){
+     
         humanChoice=humanChoice.toLowerCase();
         if ((humanChoice=='rock'&& computerChoice=='scissors')||
             (humanChoice=='scissors'&& computerChoice=='paper')||
             (humanChoice=='paper'&& computerChoice=='rock')){
-                console.log(`Round${round} winner: USER! \t ${humanChoice} beats ${computerChoice}`);
+                divText=`USER won! \t ${humanChoice} beats ${computerChoice}`;
                 humanScore+=1;
+                
             }
         else if(humanChoice===computerChoice){
-            console.log(`Round ${round} was a Tie!`);
+            divText=`Game Tie!`;
         }
         else {
-            console.log(`Round ${round} winner: COMPUTER! \t ${computerChoice} beats ${humanChoice}`);
+            divText=`COMPUTER won! \t ${computerChoice} beats ${humanChoice}`;
             computerScore+=1;
-        }                
+        }   
+        console.log(divText);       
+        round.textContent=divText;
+        score.textContent=`Computer: ${computerScore}\n Player: ${humanScore}`   ;
+        finalResult.textContent='';
+        let winner=computerScore>humanScore?"COMPUTER":"PLAYER";
+        if (computerScore>=5 || humanScore>=5){
+            finalResult.textContent= `Final Winner: ${winner}`;
+            round.textContent='';
+            computerScore=humanScore=0;
+        }
+         
+        
     }
         
     
 
     let humanScore=0;
     let computerScore=0;
-    for(let i=1;i<=5;i++){
-        let computerSel=getComputerChoice();
-        let humanSel=getHumanChoice();
-        playRound(humanSel,computerSel,i);
-    }
+    const btn=document.querySelectorAll('button');
+    const div=document.querySelector('div');
+    const round=document.createElement('p');
+    const score=document.createElement('p');
+    let finalResult=document.createElement('p');
+    let divText='';  
+    
+    btn.forEach((button)=>{
+        button.addEventListener('click',(e)=>{
+            let targetBtn=e.target;
+            let playerSelection=targetBtn.textContent;
+            let computerSelection=getComputerChoice();
+            playRound(playerSelection,computerSelection);
+            
+        })
+    })
+    div.appendChild(round);
+    div.appendChild(score);
+    div.appendChild(finalResult);
+    
+    
+       
+}
 
-    let winner;
-    if (humanScore>computerScore)
-        winner='USER';
-    else if (humanScore==computerScore)
-        {console.log("Game is a Tie!");
-        winner='NONE'
-        }
-    else
-        winner='COMPUTER';
-    console.log(`Final Score ==> User:${humanScore}\t Computer:${computerScore}`);
-    console.log(`Winner: ${winner}`);
-    }
 
 playGame();
 
